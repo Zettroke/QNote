3d70bbe32321627a530716d2c9c199e4425c226d
Zettroke <skorostnoy2000@yandex.ru>
2018-10-21 23:46:04 +0300

    Add timezone middleware for auto timezone setup from user model

--- QNoteProject/settings.py
+++ QNoteProject/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.auto_timezone.UserTimezoneAutoSetupMiddleware',
 ]
 
 ROOT_URLCONF = 'QNoteProject.urls'
---
+++ main/middleware/auto_timezone.py
@@ -0,0 +1,15 @@
+from django.utils import timezone
+
+
+class UserTimezoneAutoSetupMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        if request.user.is_authenticated:
+            timezone.activate(timezone.FixedOffset(request.user.timezone_offset))
+
+        response = self.get_response(request)
+
+        return response
